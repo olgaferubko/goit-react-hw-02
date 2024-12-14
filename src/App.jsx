3efd62aceafcd5d5cfd45f6faf9a76b95f138441
Olga Ferubko <ferubko.olga@gmail.com>
2024-12-14 21:09:26 +0200

    correct reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,63 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 import Description from "./components/Description/Description";
 import Options from "./components/Options/Options";
 import Feedback from "./components/Feedback/Feedback";
 import Notification from "./components/Notification/Notification";
 
 function App() {
-    const basicFeedback = {
-        good: 0,
-        neutral: 0,
-        bad: 0,
-    };
+  const basicFeedback = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
 
-    const [options, setOptions] = useState(() => {
-        const reviews = localStorage.getItem("reviews");
-        if (reviews !== null) {
-            return JSON.parse(reviews);
-        }
-        return basicFeedback;
-    });
+  const [options, setOptions] = useState(() => {
+    const reviews = localStorage.getItem("reviews");
+    return reviews ? JSON.parse(reviews) : basicFeedback;
+  });
 
-    useEffect(() => {
-        localStorage.setItem("reviews", JSON.stringify(options));
-    }, [options]);
+  useEffect(() => {
+    localStorage.setItem("reviews", JSON.stringify(options));
+  }, [options]);
 
-    const updateFeedback = (feedbackType = "reset") => {
-        if (feedbackType === "reset") {
-            return setOptions(() => basicFeedback);
-        }
+  const updateFeedback = (feedbackType) => {
+    setOptions((prev) => ({
+      ...prev,
+      [feedbackType]: prev[feedbackType] + 1,
+    }));
+  };
 
-        setOptions((prev) => {
-            return {
-                ...prev,
-                [feedbackType]: prev[feedbackType] + 1,
-            };
-        });
-    };
+  const resetFeedback = () => {
+    setOptions(basicFeedback);
+  };
 
-    const totalFeedback = Object.values(options).reduce((total, value) => total + value, 0);
+  const totalFeedback = Object.values(options).reduce((total, value) => total + value, 0);
 
-    return (
-        <>
-            <Description />
-            <Options
-                onUpdate={updateFeedback}
-                feedbackItems={options}
-                keepFeedback={totalFeedback}
-            />
-            {totalFeedback > 0 && (
-                <Feedback
-                    feedbackItems={options}
-                    total={totalFeedback}
-                    positiveFeedback={Math.round((options.good / totalFeedback) * 100)}
-                />
-            )}
-            {totalFeedback === 0 && <Notification />}
-        </>
-    );
+  const positiveFeedback = totalFeedback
+    ? Math.round((options.good / totalFeedback) * 100)
+    : 0;
+
+  return (
+    <>
+      <Description />
+      <Options
+        onUpdate={updateFeedback}
+        onReset={resetFeedback}
+        feedbackItems={options}
+        keepFeedback={totalFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          feedbackItems={options}
+          total={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      ) : (
+        <Notification />
+      )}
+    </>
+  );
 }
 
 export default App;
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,16 +1,15 @@
 import s from "./Options.module.css";
 
-export default function Options({ onUpdate = () => {}, feedbackItems = {}, keepFeedback = 0 }) {
+export default function Options({ onUpdate, onReset, feedbackItems, keepFeedback }) {
   return (
     <div className={s.optionsContainer}>
-      {feedbackItems && typeof feedbackItems === "object" &&
-        Object.keys(feedbackItems).map((title, id) => (
-          <button onClick={() => onUpdate(title)} className={s.option} key={id}>
-            {title}
-          </button>
-        ))}
+      {Object.keys(feedbackItems).map((title, id) => (
+        <button onClick={() => onUpdate(title)} className={s.option} key={id}>
+          {title}
+        </button>
+      ))}
       {keepFeedback > 0 && (
-        <button className={s.option} onClick={() => onUpdate()}>
+        <button className={s.option} onClick={onReset}>
           reset
         </button>
       )}
